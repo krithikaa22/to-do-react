4ad2230ed6febee30b76ef366187326f3820e925
krithikaa22 <69051821+krithikaa22@users.noreply.github.com>
2021-05-10 21:43:33 +0530

    Update Form.js

--- Form.js
+++ Form.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import 'font-awesome/css/font-awesome.min.css';
-import Complete from './Complete';
 
-function Form({todo, setTask, setTodo, task, setStatus,  setFilter})
+function Form({todo, setTask, setTodo, task, setStatus,  setFilter, edit, setEdit   })
 {
     const addTask = (e) => {
         setTask(e.target.value);
     };
     const updateTodo = (e) => {
         e.preventDefault();
-        setTodo([...todo, {text: task, id: todo.length, completed: false}]);
-        setFilter([...todo, {state: false}]);
+        if(edit !== null)
+        {
+            var arr = todo.map(t => {
+                if(edit === t.id)
+                {
+                    return {...t, text: task}
+                }
+                return t;
+            })
+            setTodo(arr);
+            setEdit(null);
+        }
+        else{
+            setTodo([...todo, {text: task, id: todo.length, completed: false}]);
+            setFilter([...todo, {state: false}]);
+        }
         setTask("");
     };
     const statusHandler = (e) => {
